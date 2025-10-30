d88b5b42e3eed3a1b135b291afea312e3d1bf9d8
nishizawa <gm.nishizawa@gamil.com>
2025-10-30 17:31:33 +0900

    Cloudflare Web Analytics追加

---
+++ static/js/analytics.js
@@ -0,0 +1,7 @@
+(function() {
+  var script = document.createElement('script');
+  script.defer = true;
+  script.src = 'https://static.cloudflareinsights.com/beacon.min.js';
+  script.setAttribute('data-cf-beacon', '{"token": "e63686535d3b43ada0ad2b4f1713c906"}');
+  document.head.appendChild(script);
+})();
